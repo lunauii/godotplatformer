b6ac4dc58822208118d84586c1c93ac5cfad7f44
lunaui <65362444+Lunauii@users.noreply.github.com>
2022-11-25 09:47:16 -0800

    Updated code of plr_base.gd

--- clc2/plr_base.gd
+++ clc2/plr_base.gd
@@ -1,13 +1,41 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
+var jumpsLeft = 0
+const SPEED = 25
+const GRAVITY = 20
+const JUMPFORCE = -900
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = 150
-	if Input.is_action_pressed("left"):
-		velocity.x = -150
-		
-	move_and_slide(velocity)
 	
-	velocity.x = lerp(velocity.x, 0, 0.15)
+	velocity.y = velocity.y + 30
+	
+	if Input.is_action_pressed("right"):
+		velocity.x += SPEED
+		velocity.x *= pow(1.0, delta)
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
+		velocity.x += -SPEED
+		velocity.x *= pow(1.0, delta)
+		$Sprite.play("walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("idle")
+		
+	print(velocity.x)
+		
+	if not is_on_floor():
+		$Sprite.play("jump")
+	elif is_on_floor():
+		jumpsLeft = 1
+		
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
+	elif Input.is_action_just_pressed("jump") and jumpsLeft != 0:
+		velocity.y = JUMPFORCE
+		jumpsLeft -= 1
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	velocity.x = lerp(velocity.x, 0, 0.1)
