b1bba80c88cd7ddc2c1ad36d312748759beccdc8
lunaui <65362444+Lunauii@users.noreply.github.com>
2022-12-06 14:36:50 -0800

    Update HUD.gd

--- clc2/HUD.gd
+++ clc2/HUD.gd
@@ -3,21 +3,15 @@ extends CanvasLayer
 var coins = 0
 var winCondition = 3
 
-func _physics_process(delta):
-	if coins == winCondition:
-		get_tree().change_scene("res://scn_level1.tscn")
-		
-		
-		
 func _ready():
 	$Coins.text = String(coins)
-	
-	
 	
 func _on_coin_collected():
 	coins += 1
 	print("Coins: ", coins)
 	_ready()
+	if coins == winCondition:
+		get_tree().change_scene("res://scn_win.tscn")
 
 
 
