2c0e0be2464105a07364ca6a066bab8ad9ffafdb
lunaui <angelo.rws@gmail.com>
2022-12-14 15:01:16 -0800

    Removed excess files from the origin tree

--- gobkac.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-
-
-
-func _on_gobkac_pressed():
-	get_tree().change_scene("res://scn_level1.tscn")
--- playwithmeuwu.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-
-
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://scn_level1.tscn")
